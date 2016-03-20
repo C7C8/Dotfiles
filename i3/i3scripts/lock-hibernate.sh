318d04fa162d9ee3883aaa3408069d42770c3b0d
Sourec <tothemyers@gmail.com>
2016-03-20 19:31:43 -0400

    Clean up conky, add conky date/clock

--- i3/i3scripts/lock-hibernate.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo pm-hibernate & i3lock -i ~/Pictures/Custom_Wallpapers/Red_Beams_LOCK.png
--- i3/i3scripts/lock-suspend.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-i3lock -i ~/Pictures/Custom_Wallpapers/Red_Beams_LOCK.png
-sudo pm-suspend
