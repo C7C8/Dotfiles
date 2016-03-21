ec3096d1d828f1eff5948075b6fb3693fb7937eb
Sourec <tothemyers@gmail.com>
2016-03-21 10:39:17 -0400

    Add mode switching capability to conky+desktop, "BATTLE MODE"

---
+++ modes/switch_battle.sh
@@ -0,0 +1,4 @@
+killall conky
+feh --bg-scale /home/sourec/Dotfiles/modes/battlemode/Battle_Mode_HUD.png
+conky -c /home/sourec/Dotfiles/modes/battlemode/conkyrc & exit
+
---
+++ modes/switch_standby.sh
@@ -0,0 +1,3 @@
+killall conky
+feh --bg-scale /home/sourec/Dotfiles/modes/standby/Spaceship_Beam.jpg
+conky -c /home/sourec/Dotfiles/modes/standby/conkyrc & exit
