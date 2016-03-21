b85ee29e489bff94ee0810f2d4837176f257b3fb
Sourec <tothemyers@gmail.com>
2016-03-21 17:34:20 -0400

    Add battlemode transition

--- modes/switch_battle.sh
+++ modes/switch_battle.sh
@@ -1,4 +1,14 @@
 killall conky
-feh --bg-scale /home/sourec/Dotfiles/modes/battlemode/Battle_Mode_HUD.png
+feh --bg-scale /home/sourec/Dotfiles/modes/battlemode/raw.png
+sleep 0.15
+feh --bg-scale /home/sourec/Dotfiles/modes/battlemode/outer.png
+sleep 0.15
+feh --bg-scale /home/sourec/Dotfiles/modes/battlemode/middle.png
+sleep 0.15
+feh --bg-scale /home/sourec/Dotfiles/modes/battlemode/inner.png
+sleep 0.15
+feh --bg-scale /home/sourec/Dotfiles/modes/battlemode/full.png
+sleep 0.15
+feh --bg-scale /home/sourec/Dotfiles/modes/battlemode/hud.png
 conky -c /home/sourec/Dotfiles/modes/battlemode/conkyrc & exit
 
