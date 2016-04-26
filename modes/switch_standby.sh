8cd412db8a7e3ede12ea41e9227b6481621c9256
Sourec <tothemyers@gmail.com>
2016-04-26 07:08:06 -0400

    Change desktop bg, change conky storage/connection config

--- modes/switch_standby.sh
+++ modes/switch_standby.sh
@@ -1,3 +1,3 @@
 killall conky
-feh --bg-scale /home/sourec/Dotfiles/modes/standby/Spaceship_Beam.jpg
+feh --bg-scale /home/sourec/Dotfiles/modes/standby/Debian_Stars.jpg
 conky -c /home/sourec/Dotfiles/modes/standby/conkyrc & exit
