a3839974165607318793a17c7d7179bf9a2db8bb
Sourec <tothemyers@gmail.com>
2017-07-24 19:07:07 -0400

    Refactor media handling

---
+++ scripts/setwallpaper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Right now this script is a standin for a real script, because my wallpaper
+# is currently just static. That's going to change in the future, where I
+# will compose a wallpaper on the fly using various elements like the
+# wallpaper base or HUD elements as desired. 
+feh --bg-scale --no-xinerama --no-fehbg ~/dotfiles/media/wallpaper-base.png
+
