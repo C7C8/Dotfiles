e63276bc9f3e3a82e708489cb94c9ee58d95c1d5
Sourec <tothemyers@gmail.com>
2017-08-23 14:08:58 -0400

    Resize i3lock screen to fit 1366x768 instead of 1920x1080

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-i3lock  --insidecolor=00000000 --ringcolor=00000000 --keyhlcolor=FF000059 --bshlcolor=FF000059 --linecolor=00000000 --textcolor=FFFFFFFF --ringwrongcolor=00000000 --ringvercolor=00000000 --insidewrongcolor=00000000 --insidevercolor=00000000  -i ~/dotfiles/media/lockscreen.png -B 10 -S DP-2
+i3lock  --insidecolor=00000000 --ringcolor=00000000 --keyhlcolor=FF000059 --bshlcolor=FF000059 --linecolor=00000000 --textcolor=FFFFFFFF --ringwrongcolor=00000000 --ringvercolor=00000000 --insidewrongcolor=00000000 --insidevercolor=00000000 -B 10 --radius 64 -i ~/dotfiles/media/lockscreen.png
