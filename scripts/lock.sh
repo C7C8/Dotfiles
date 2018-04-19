3f44675ca4e176ea989fd788e46396b118d155e2
Sourec <tothemyers@gmail.com>
2018-04-19 08:57:51 -0400

    Fix i3lock script

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-i3lock  --insidecolor=00000000 --ringcolor=00000000 --keyhlcolor=FF000059 --bshlcolor=FF000059 --linecolor=00000000 --textcolor=FFFFFFFF --ringwrongcolor=00000000 --ringvercolor=00000000 --insidewrongcolor=00000000 --insidevercolor=00000000  -i ~/dotfiles/media/lockscreen.png -B 10
+i3lock  --insidecolor=00000000 --ringcolor=00000000 --keyhlcolor=FF000059 --bshlcolor=FF000059 --linecolor=00000000 --ringwrongcolor=00000000 --ringvercolor=00000000 --insidewrongcolor=00000000 --insidevercolor=00000000  -i ~/dotfiles/media/lockscreen.png -B 10
 
